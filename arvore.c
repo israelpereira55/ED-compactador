0e3b0d92c481e08891321fa890d3033a37a91397
israelpereira55 <israelpereira55@gmail.com>
2016-05-27 16:38:59 -0300

    added arvore.c

---
+++ arvore.c
@@ -0,0 +1,33 @@
+struct arv {
+//	Item* item;
+	Arv* esq,
+	     dir;
+};
+
+Arv* arv_inicializa() {
+	
+	Arv* a = (Arv*) malloc (sizeof(Arv));
+	a->esq = NULL;
+	a->dir = NULL;
+	return a;
+}
+
+Arv* arv_libera(Arv* a) {
+	
+	if (a == NULL) {
+		return NULL;
+	}
+
+	if (a->esq != NULL) {
+		arv_libera(a->esq);
+	}
+
+	if(a->dir != NULL) {
+		arv_libera(a->dir);
+	}
+
+	free(a);
+	return NULL;
+}
+
+		

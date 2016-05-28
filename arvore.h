13ea87047602ee4da96b2026f1e5e5b22d5d8e45
israelpereira55 <israelpereira55@gmail.com>
2016-05-28 16:33:38 -0300

    arvore.h updated

--- arvore.h
+++ arvore.h
@@ -7,9 +7,11 @@ Arv* arv_inicializa_unica(char c);
 
 Arv* arv_inicializa(char c, Arv* esq, Arv* dir);
 
-Arv* arv_pai (Arv* a, char c);
+Arv* arv_inicializa_huffman(int i, Arv* esq, Arv* dir);
 	
 int arv_pertence (Arv* a, char c);
+
+Arv* arv_pai (Arv* a, char c);
 
 Arv* arv_retira (Arv* a, char c);
 
@@ -18,5 +20,17 @@ Arv* arv_busca(Arv* a, char c);
 Arv* arv_libera(Arv* a);
 
 char info (Arv* a);
+
+Arv* arv_troca(Arv* a);
+
+Arv* arv_ordena(Arv* a);
+
+Arv* arv_insere (Arv* original, Arv* adicional);
+
+Arv* arv_huffman (Arv* lista);
+
+Arv* arv_cria_lista_frequencia (int* freq);
+
+void arv_imprime_lista (Arv* lista);
 
 #endif 
